5895769678054098ed7a2d9310f0fca00581b35a
Coconutu <sebastian.daniel.adrian@gmail.com>
2025-12-14 06:38:09 +0200

    simplificare functie genereazaTabla()

--- script.js
+++ script.js
@@ -1,17 +1,16 @@
-const joc=document.getElementById('joc');
-const btnReset=document.getElementById('btnReset');
+const joc = document.getElementById('joc');
+const btnReset = document.getElementById('btnReset');
 genereazaTabla();
-function genereazaTabla(){
-    let l,c;
-    for(let i=0;i<9;i++){
-        let e=document.createElement('div');
-        l=Math.round((i+2)/3)-1; 
-        c=Math.round(i%3);
-        e.setAttribute('l',l);
-        e.setAttribute('c',c);
-        joc.appendChild(e);
+
+function genereazaTabla() {
     
+    for (let l = 0; l < 3; l++) {
+        for (let c = 0; c < 3; c++) {
+            let e = document.createElement('div');
+            e.setAttribute('l', l);
+            e.setAttribute('c', c);
+            joc.appendChild(e);
+        }
     }
 }
-
 // https://www.youtube.com/watch?v=iw8TH9ZgtGM&list=PLTwWgdVvyeRodZrDNJDtMpu81xuXNQpAd&index=26
