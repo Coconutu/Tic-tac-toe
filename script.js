1c395ac5106b166e59df7bf4a57d54d9a8bcaad6
Coconutu <sebastian.daniel.adrian@gmail.com>
2025-12-14 07:02:52 +0200

    update

--- script.js
+++ script.js
@@ -1,9 +1,32 @@
 const joc = document.getElementById('joc');
 const btnReset = document.getElementById('btnReset');
-genereazaTabla();
+let jucator = "X", mutari = 0;
+let tabla = [[null, null, null],
+[null, null, null],
+[null, null, null]];
 
+genereazaTabla();
+joc.addEventListener('click', (e) => {
+    const th = e.target;
+    let l = parseInt(e.target.getAttribute('l'));
+    let c = parseInt(e.target.getAttribute('c'));
+    if (tabla[l][c])
+        return;
+    tabla[l][c] = jucator;
+    tg.innerHtml = jucator;
+    mutari++;
+    if (gameOver(l, c, jucator)) {
+        alert(`Jucatorul ${jucator} a castigat!`);
+        btnReset.disabled = false;
+    } else if (mutari == 9) {
+            alert("Remiza!");
+            btnReset.disabled = false;
+        } else {
+            schimbaJucator();
+        }
+    });
 function genereazaTabla() {
-    
+
     for (let l = 0; l < 3; l++) {
         for (let c = 0; c < 3; c++) {
             let e = document.createElement('div');
@@ -13,4 +36,10 @@ function genereazaTabla() {
         }
     }
 }
+
+
+
+
+
+
 // https://www.youtube.com/watch?v=iw8TH9ZgtGM&list=PLTwWgdVvyeRodZrDNJDtMpu81xuXNQpAd&index=26
